feedd0af37084dd10ae6d73957bec385bae14ef0
brilliantchikanya <brilliant.chikanya@gmail.com>
2024-11-22 01:05:49 +0200

    some minor changes

--- DSA/src/com/bullet/dsa/Person.java
+++ DSA/src/com/bullet/dsa/Person.java
@@ -84,6 +84,6 @@ public class Person {
     public static void main(String[] args) {
         Name myName = new Name("Trevor");
         Person person = new Person(myName, Gender.MALE);
-        System.out.println(person);
+        System.out.println(person); // Trevor   MALE
     }
 }
